c66196c79a55254bfc5af220b2cb6a3eb2236422
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2023-01-01 18:53:29 +0530

    Change openzepplin and solidity version

--- contracts/JoviToken.sol
+++ contracts/JoviToken.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol"; 
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol"; 
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol"; 
 import "hardhat/console.sol";
 
-contract JoviToken is ERC20 {
+contract JoviToken is ERC20Mintable, ERC20Pausable, ERC20Detailed {
 
-    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) public {
+    constructor(string memory name_, string memory symbol_, uint8 decimals) ERC20Detailed(name_, symbol_, decimals) public {
         console.log("New JoviToken created");
     }
 }
