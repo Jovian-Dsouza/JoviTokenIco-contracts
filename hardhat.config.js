b5dd91c6e0f5850a7c0ee8e7a117329d38554b1e
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2023-01-07 22:21:12 +0530

    Add goerli testnet to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
 require('babel-register');
 require('babel-polyfill');
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.5.0",
+  networks: {
+    goerli: {
+      url: process.env.QUICK_NODE_URL,
+      accounts: [
+                  process.env.GOERLI_PRIVATE_KEY
+                ]
+    },
+  },
 };
