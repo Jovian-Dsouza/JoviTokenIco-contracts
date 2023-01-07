a8e0968dc08300bb5457905b77184bfb8e3248f3
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2023-01-07 18:32:34 +0530

    Remove redundant console.log() form token

--- contracts/JoviToken.sol
+++ contracts/JoviToken.sol
@@ -4,11 +4,9 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol"; 
 import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol"; 
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol"; 
-import "hardhat/console.sol";
 
 contract JoviToken is ERC20Mintable, ERC20Pausable, ERC20Detailed {
 
     constructor(string memory name_, string memory symbol_, uint8 decimals) ERC20Detailed(name_, symbol_, decimals) public {
-        console.log("New JoviToken created");
     }
 }
